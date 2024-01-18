facd202e1fd213b7f6221c9c1795c5a291d36a0c
vikas <vikasisgen@gmail.com>
2024-01-18 17:27:53 +0530

    address all routes set

--- controllers/address.js
+++ controllers/address.js
@@ -3,10 +3,8 @@ const User = require('../modals/user');
 const getAddress = async (req, res) => {
     try {
         const { userId } = req.params;
-        if (userId.length < 10) {
-            res.json({ status: 'empty' })
-            return
-        }
+        if (userId.length < 10) { res.json({ status: 'empty' }); return };
+
         const user = await User.findById(userId);
         res.json({ status: 'true', address: user.address })
     } catch (error) {
@@ -17,10 +15,8 @@ const getAddress = async (req, res) => {
 const addAddress = async (req, res) => {
     try {
         const { userId, address } = req.body;
-        if (userId.length < 10) {
-            res.json({ status: 'empty' })
-            return
-        }
+        if (userId.length < 10) { res.json({ status: 'empty' }); return };
+
         const user = await User.findById(userId);
         user.address.push(address);
         await user.save()
@@ -30,5 +26,36 @@ const addAddress = async (req, res) => {
         res.json({ status: 'error' })
     }
 }
+const deleteAddress = async (req, res) => {
+    try {
+        const { userId, addressId } = req.body;
+        if (userId.length < 10) { res.json({ status: 'empty' }); return };
 
-module.exports = { getAddress, addAddress }
+        const user = await User.findById(userId);
+        var index = user.address.findIndex((state) => state._id === addressId);
+        user.address.splice(index, 1);
+        await user.save()
+        res.json({ status: 'true' })
+    } catch (error) {
+        console.log(error);
+        res.json({ status: 'error' })
+    }
+}
+const editAddress = async (req, res) => {
+    try {
+        const { userId, addressId, address } = req.body;
+        if (userId.length < 10) { res.json({ status: 'empty' }); return };
+
+        const user = await User.findById(userId);
+        var index = user.address.findIndex((state) => state._id === addressId);
+        user.address.splice(index, 1);
+        user.address.push(address)
+        await user.save()
+        res.json({ status: 'true' })
+    } catch (error) {
+        console.log(error);
+        res.json({ status: 'error' })
+    }
+}
+
+module.exports = { getAddress, addAddress, deleteAddress, editAddress }
--- routes/address.js
+++ routes/address.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAddress, addAddress } = require('../controllers/address');
+const { getAddress, addAddress, deleteAddress, editAddress } = require('../controllers/address');
 
 const router = express.Router();
 
 router.route('/fetch/:userId').get(getAddress);
 router.route('/add').post(addAddress);
+router.route('/delete').post(deleteAddress);
+router.route('/edit').post(editAddress);
 
 module.exports = router;
